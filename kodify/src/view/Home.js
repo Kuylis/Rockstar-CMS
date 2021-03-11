4e8a23b42f726814c6039021d91453904aaa0d93
Frida333 <lindstromfrida@yahoo.se>
2021-03-11 11:00:36 +0100

    add router

--- kodify/src/view/Home.js
+++ kodify/src/view/Home.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import { Helmet } from 'react-helmet-async';
+
 
 const Home = () => {
     return (
         <div>
-            
+          <Helmet>
+            <title>Konsultbolag i Malmö inom DevOps, Cloud och Management- kodify kodify.se</title>
+          </Helmet>
         </div>
     )
 }
