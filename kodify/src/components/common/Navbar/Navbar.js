3e64b81eeb2d2dc07659d47eb6ec99649fac6760
Cecilia <cecilia.svensson.is@gmail.com>
2021-03-17 11:15:28 +0100

    working navbar - responsive, one breakpoint

--- kodify/src/components/common/Navbar/Navbar.js
+++ kodify/src/components/common/Navbar/Navbar.js
@@ -1,7 +1,8 @@
-import './Navbar.scss';
+import '../../../scss/Main.scss';
+
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { Button } from '../Button';
+import { Button } from '../../component/Button';
 
 const Navbar = () => {
     const [click, setClick] = useState(false);
@@ -44,11 +45,11 @@ const Navbar = () => {
                     <Link to='/'className='navbar__logo' onClick={closeMobileMenu}>
                         <img src='https://kodify.se/_files/200000055-1136111363/200/B3%20Kodify_Positiv.png' alt='B3 kodify logo' />
                     </Link>
-                    <div className='nav__menu-icon' onClick={handleClick}>
+                    <div className='navbar__menu-icon' onClick={handleClick}>
                         <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
                     </div>
                 </div>
-                <ul className={click ? 'nav__menu--active' : 'nav__menu'}>
+                <ul className={click ? 'nav__menu active' : 'nav__menu'}>
                     <li className='nav__item'>
                         <Link to='/' className='nav__links'>Hem</Link>
                     </li>
@@ -64,9 +65,11 @@ const Navbar = () => {
                     <li className='nav__item'>
                         <Link to='/nyheter' className='nav__links' onClick={closeMobileMenu}>Blogg och nyheter</Link>
                     </li>
+                    <li>
+                        <Link to='/' className='nav__links__mobile' onClick={closeMobileMenu}>SIGN IN</Link>
+                    </li>
                 </ul>
                 {button && <Button buttonStyle='btn--outline'>SIGN IN</Button>}
-                
             </nav>
         </>
     )
